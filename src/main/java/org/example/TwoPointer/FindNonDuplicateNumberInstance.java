12b81e0f6ab184e8705406826fd203be45b9e4da
shakibaenur <shakibaenur2612@gmail.com>
2023-10-29 16:49:12 +0600

    solved NumberOfGoodPairs leetcode 1512

---
+++ src/main/java/org/example/TwoPointer/FindNonDuplicateNumberInstance.java
@@ -0,0 +1,28 @@
+package org.example.TwoPointer;
+
+public class FindNonDuplicateNumberInstance {
+
+
+        public static int remove(int[] arr) {
+            int nextNonDuplicate = 1; // index of the next non-duplicate element
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[nextNonDuplicate - 1] != arr[i]) {
+                    arr[nextNonDuplicate] = arr[i];
+                    nextNonDuplicate++;
+                }
+            }
+
+            return nextNonDuplicate;
+        }
+
+        public static void main(String[] args) {
+            int[] arr = new int[] { 2, 3, 3, 3, 6, 9, 9 };
+            //System.out.println(remove(arr));
+
+            arr = new int[] { 2, 2, 2, 11 };
+            //System.out.println(remove(arr));
+        }
+
+    //T.C: O(N)
+    //S.C: O(1)
+}
