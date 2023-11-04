50c4e0e185e88d15c8492c4f23658a9938059454
shakibaenur <shakibaenur2612@gmail.com>
2023-11-04 20:36:33 +0600

    solved 141. Linked List Cycle

--- src/main/java/org/example/FastSlowPointer/LinkedListCycle.java
+++ src/main/java/org/example/FastSlowPointer/LinkedListCycle.java
@@ -1,28 +1,42 @@
 package org.example.FastSlowPointer;
 
 public class LinkedListCycle {
-    /**
-     * Definition for singly-linked list.
-     * class ListNode {
-     *     int val;
-     *     ListNode next;
-     *     ListNode(int x) {
-     *         val = x;
-     *         next = null;
-     *     }
-     * }
-     */
+
+  //Here we have used Fast & Slow pointer approach/Hare & Tortoise algorithm
+    //The fast pointer is one step behind the slow pointer.
+  //The fast pointer is two steps behind the slow pointer.
+   class ListNode {
+       int val;
+       ListNode next;
+       ListNode(int x) {
+           val = x;
+           next = null;
+       }
+   }
     public class Solution {
-//        public boolean hasCycle(ListNode head) {
-//            ListNode slow = head;
-//            ListNode fast = head;
-//            while (fast != null && fast.next != null) {
-//                fast = fast.next.next;
-//                slow = slow.next;
-//                if (slow == fast)
-//                    return true; // found the cycle
-//            }
-//            return false;
-//        }
+        public boolean hasCycle(ListNode head) {
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (slow == fast)
+                    return true; // found the cycle
+            }
+            return false;
+        }
     }
 }
+
+/*Time Complexity
+As we have concluded above,
+ once the slow pointer enters the cycle,
+ the fast pointer will meet the slow pointer in the same loop.
+ Therefore, the time complexity of our algorithm will be O(N) where ‘N’
+ is the total number of nodes in the LinkedList.
+
+Space Complexity
+
+The algorithm runs in constant space O(1).
+
+*/
